1a67d3ec88b8b0071e3b5bf3842ae1fbb65a715f
sebastian <sebastian.sala.tech@gmail.com>
2026-08-19 01:54:57 -0600

    Preserve tick count constraints in SQLite migrations

--- db/migrate/20220706101937_change_runs_tick_columns_to_bigints.rb
+++ db/migrate/20220706101937_change_runs_tick_columns_to_bigints.rb
@@ -3,14 +3,14 @@
 class ChangeRunsTickColumnsToBigints < ActiveRecord::Migration[7.0]
   def up
     change_table(:maintenance_tasks_runs, bulk: true) do |t|
-      t.change(:tick_count, :bigint)
+      t.change(:tick_count, :bigint, default: 0, null: false)
       t.change(:tick_total, :bigint)
     end
   end
 
   def down
     change_table(:maintenance_tasks_runs, bulk: true) do |t|
-      t.change(:tick_count, :integer)
+      t.change(:tick_count, :integer, default: 0, null: false)
       t.change(:tick_total, :integer)
     end
   end
